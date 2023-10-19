fe9c09701a9b888115f4e2036ce5efa26523a03a
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-19 18:54:54 -0300

    :fire: fix: error to apply global theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './Theme.jsx'
+import { GlobalStyle } from './GlobalStyle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={ theme } >
-      <App />
-    </ThemeProvider>
+    <GlobalStyle />
+      <ThemeProvider theme={ theme } >
+        <App />
+      </ThemeProvider>
   </React.StrictMode>,
 )
