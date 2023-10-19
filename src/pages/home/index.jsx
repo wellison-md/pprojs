288e9b6e36e37480ac2cbbee2cac0bb09a8e3d14
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-19 18:43:46 -0300

    :recycle: feat: updated home page comp

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,49 @@
 import Flex from "../../components/common/flex";
 import Spacer from "../../components/common/spacer";
+import illustration1 from "../../assets/ilustration1.png";
+import logowmd from "../../assets/logowmd.svg";
+import HeroLabel from "../../components/heroLabel";
+import Header from "../../components/header";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <Header />
+      <Spacer $s="36px"/>
+
+      <nav>
         <Flex $h="end">
-          <button>Light-mode</button>
-          <button>PT-BR</button>
-          <button>EN-US</button>
+          <button>início</button>
+          <button>contato</button>
+          <button>projetos</button>
+          <button>sobre</button>
         </Flex>
-        <Spacer />
-        <h1>Hello world</h1>
-      </header>
+      </nav>
+
+      <Flex>
+      <section>
+        <Flex>
+          <img width="100px" src={ logowmd } alt="logo" />
+
+          <section>
+           <HeroLabel
+              title="Wellison M. Dias"
+              subTitle="<Desenvolvedor Web/>"
+            ></HeroLabel>
+          </section>
+
+        </Flex>
+        <p>
+          Olá! sou o Wellison /ué-li-som/ programador web apaixonado por transformar ideias em código e criar soluções incríveis com software. Estou pronto para levar seus projetos para o próximo nível. Se você está em busca de websites funcionais, elegantes e totalmente responsivos, você veio ao lugar certo. Dê uma olhada nos meus projetos e descubra como posso fazer a diferença para o seu próximo projeto web
+
+        </p>
+      </section>
+
+      <section>
+        <img src={ illustration1 } alt="illustration"/>
+      </section>
+
+      </Flex>
 
     </>
   );
