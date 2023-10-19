7cd27ef5c53084735dd00a6dc09ed3128261e35c
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-19 15:16:42 -0300

    :zap: feat: create initial global style

---
+++ src/Theme.jsx
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    main: '#020617',
+    secondary: '#F36600',
+    text1: '#87B3FB',
+    text2: '#4D6282',
+  },
+  sizes: {
+    x: '',
+    m: '',
+    l: '',
+    xl: '',
+  },
+  screens: {
+    sm: '',
+    md: '',
+    lg: '',
+  },
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './Theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ theme } >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
