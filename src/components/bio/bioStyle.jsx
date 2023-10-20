3a9e3c597b848b57b9867321dd313b475e148211
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-19 23:32:10 -0300

    :zap: feat: create bio-comp

---
+++ src/components/bio/bioStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  max-width: 700px;
+  margin: 64px 0;
+  text-align: justify;
+  text-indent: 36px;
+  line-height: 1.5;
+  font-size: 22px;
+`;
---
+++ src/components/bio/index.jsx
@@ -0,0 +1,9 @@
+import { Text } from "./bioStyle";
+
+export default function Bio() {
+  return (
+    <Text>
+      Olá! sou o Wellison <i>/ué-li-som/</i> programador web apaixonado por transformar ideias em código e criar soluções incríveis com software. Estou pronto para levar seus projetos para o próximo nível. Se você está em busca de websites funcionais, elegantes e totalmente responsivos, você veio ao lugar certo. Dê uma olhada nos meus projetos e descubra como posso fazer a diferença para o seu próximo projeto web
+    </Text>
+  );
+}
