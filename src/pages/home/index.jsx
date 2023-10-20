be2087eccf51d6799d8e39e217de2258e41a0455
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-19 23:33:47 -0300

    :recycle: feat: updated home page-comp

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,44 +1,44 @@
 import Flex from "../../components/common/flex";
 import Spacer from "../../components/common/spacer";
-import illustration1 from "../../assets/ilustration1.png";
+import illustration1 from "../../assets/ilustration1.svg";
 import logowmd from "../../assets/logowmd.svg";
 import HeroLabel from "../../components/heroLabel";
 import Header from "../../components/header";
 import NavBar from "../../components/navBar";
+import Bio from "../../components/bio";
+import Button from "../../components/common/button";
+import Bg from "../../components/bg";
 
 export default function Home() {
   return (
     <>
+      <Bg />
       <Header />
-      <Spacer $s="36px"/>
+      <Spacer $s="48px" />
       <NavBar />
-      <Spacer $s="36px"/>
+      <Spacer $s="56px" />
 
       <Flex>
-      <section>
-        <Flex>
-          <img width="100px" src={ logowmd } alt="logo" />
+        <section>
+          <Flex>
+            <img width="110px" src={logowmd} alt="logo" />
 
-          <section>
-           <HeroLabel
-              title="Wellison M. Dias"
-              subTitle="<Desenvolvedor Web/>"
-            ></HeroLabel>
-          </section>
+            <section>
+              <HeroLabel
+                title="Wellison M. Dias"
+                subTitle="<Desenvolvedor Web/>"
+              ></HeroLabel>
+            </section>
+          </Flex>
 
-        </Flex>
-        <p>
-          Olá! sou o Wellison /ué-li-som/ programador web apaixonado por transformar ideias em código e criar soluções incríveis com software. Estou pronto para levar seus projetos para o próximo nível. Se você está em busca de websites funcionais, elegantes e totalmente responsivos, você veio ao lugar certo. Dê uma olhada nos meus projetos e descubra como posso fazer a diferença para o seu próximo projeto web
+          <Bio />
+          <Button $label="Projetos" />
+        </section>
 
-        </p>
-      </section>
-
-      <section>
-        <img src={ illustration1 } alt="illustration"/>
-      </section>
-
+        <section>
+          <img width="750px" src={illustration1} alt="illustration" />
+        </section>
       </Flex>
-
     </>
   );
 }
