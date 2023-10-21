d6dda626935392ee876709e3e495cf879bbec705
Wellison M. Dias <wellisonmdias@gmail.com>
2023-10-20 22:57:46 -0300

    :zap: feat: create initial translations

---
+++ src/locale/ptBr.js
@@ -0,0 +1,28 @@
+const PT_BR = {
+  pages: {
+    home: {
+      navbar: {
+        label1: 'início',
+        label2: 'contato',
+        label3: 'projetos',
+        label4: 'sobre'
+      },
+      role: '<Desenvolvedor Web/>',
+      bio: `
+        Olá! sou o Wellison /ué-li-som/ programador web apaixonado por transformar ideias em código e criar soluções incríveis com software. Estou pronto para levar seus projetos para o próximo nível. Se você está em busca de websites funcionais, elegantes e totalmente responsivos, você veio ao lugar certo. Dê uma olhada nos meus projetos e descubra como posso fazer a diferença para o seu próximo projeto web
+      `,
+      ctaLabel: 'Projetos',
+    },
+    contact: {
+
+    },
+    projects: {
+
+    },
+    about: {
+
+    },
+  }
+}
+
+export default PT_BR;
